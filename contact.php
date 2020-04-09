39d9e98619dd9f6eed3f83a812a8e23fce27a9a9
Jug <rj.in.19@gmail.com>
2020-04-09 23:33:07 +0800

    Add initial portfolio website

---
+++ contact.php
@@ -0,0 +1,7 @@
+<?php
+echo $_POST["recipient-email"];
+echo $_POST["message-text"];
+// if($_POST["message"]) {
+    // mail("your@email.address", "Form to email message", $_POST["message"], "From: an@email.address");
+// }
+?>
