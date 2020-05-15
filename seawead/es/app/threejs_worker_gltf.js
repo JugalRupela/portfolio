c894ece82588921a9c64f19e6757ec6a506b53c0
Jug <rj.in.19@gmail.com>
2020-05-16 03:44:39 +0800

    gltf1

--- seawead/es/app/threejs_worker_gltf.js
+++ seawead/es/app/threejs_worker_gltf.js
@@ -98,7 +98,7 @@ function start( container, marker, video, input_width, input_height, canvas_draw
     /* Load Model */
     var threeGLTFLoader = new THREE.GLTFLoader();
 
-    threeGLTFLoader.load("../Data/models/scene.gltf", function (gltf) {
+    threeGLTFLoader.load("../Data/models/gift/scene.gltf", function (gltf) {
             model = gltf.scene.children[0];
             model.position.z = 0;
             model.position.x = 100;
