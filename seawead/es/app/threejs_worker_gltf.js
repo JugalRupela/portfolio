7f26ff9fde83c4cb00191578d64b4575812bd4ee
Jug <rj.in.19@gmail.com>
2020-05-16 04:01:51 +0800

    gltf5

--- seawead/es/app/threejs_worker_gltf.js
+++ seawead/es/app/threejs_worker_gltf.js
@@ -69,7 +69,7 @@ function start( container, marker, video, input_width, input_height, canvas_draw
 
     var scene = new THREE.Scene();
 
-    var camera = new THREE.PerspectiveCamera();
+    var camera = new THREE.Camera();
     camera.matrixAutoUpdate = false;
     // var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
     // camera.position.z = 400;
